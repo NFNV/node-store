acb13edf4706ac2c7f111ad105833b909636cb5f
Nicolás Valiconto <nvaliconto@outlook.com>
2022-12-06 20:28:04 -0300

    validatorhandler done

---
+++ services/userService.js
@@ -0,0 +1,62 @@
+const faker = require("faker")
+const boom = require("@hapi/boom")
+
+class UsersService {
+  constructor() {
+    ;(this.users = []), this.generate()
+  }
+
+  generate() {
+    const limit = 100
+
+    for (let i = 0; i < limit; i++) {
+      this.users.push({
+        id: faker.datatype.uuid(),
+        name: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        job: faker.name.jobArea(),
+      })
+    }
+  }
+
+  create(data) {
+    const newUser = {
+      id: faker.datatype.uuid(),
+      ...data,
+    }
+    this.users.push(newUser)
+    return newUser
+  }
+
+  find() {
+    return this.users
+  }
+
+  findOne(id) {
+    return this.users.find((item) => item.id === id)
+  }
+
+  update(id, changes) {
+    const index = this.users.findIndex((item) => item.id === id)
+    if (index === -1) {
+      throw boom.notFound("Not found")
+    }
+    const user = this.users[index]
+    this.users[index] = {
+      ...user,
+      ...changes,
+    }
+    return this.users[index]
+  }
+
+  delete(id) {
+    const index = this.users.findIndex((item) => item.id === id)
+    if (index === -1) {
+      throw boom.notFound("Not found")
+    }
+    this.users.splice(index, 1)
+    return { id }
+  }
+}
+
+module.exports = UsersService
