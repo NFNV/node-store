e11f8e8ff0fb1bd251ae51c60aa60266731416a4
Nicolás Valiconto <nvaliconto@outlook.com>
2023-01-05 16:46:47 -0300

    passport

--- services/userService.js
+++ services/userService.js
@@ -1,5 +1,6 @@
 const boom = require("@hapi/boom")
 const { models } = require("../libs/sequelize")
+const bcrypt = require("bcrypt")
 
 class UsersService {
   constructor() {
@@ -7,7 +8,9 @@ class UsersService {
   }
 
   async create(data) {
-    const newUser = await models.User.create(data)
+    const hash = await bcrypt.hash(data.password, 10)
+    const newUser = await models.User.create({ ...data, password: hash })
+    delete newUser.dataValues.password
     return newUser
   }
 
